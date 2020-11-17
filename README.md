49bc4f1afbe6a1a78fec38c43c8910766b0e2148
YangWenlong123 <496192903@qq.com>
2020-11-17 18:00:03 +0800

    feat: 完善类型检测

--- README.md
+++ README.md
@@ -1,16 +1,61 @@
-## 介绍
-    常用函数工具库
->
 
+前端业务代码工具库
 
-## 安装使用
+> 目的：高效率完成前端业务代码
 
+业务开发过程中，会经常用到`日期格式化`、`url参数转对象`、`浏览器类型判断`、`节流函数`等常用函数，为避免不同项目多次复制粘贴的麻烦，这里统一封装，并发布到npm，以提高开发效率。如果你也有常用的代码，欢迎为本项目提交pr。
+
+##  安装使用
+
+1. 直接下载目录下的[index.js](https://github.com/YangWenLong123/util/blob/master/index.js)使用，支持UMD通用模块规范
+2. 使用npm安装
+
+### 浏览器:
+``` html
+  <script src="index.js"></script>
+  <script>
+      const flag = Util.isObject({})
+  </script>
 ```
-    使用npm安装：npm install -s util_alg
 
-    通过es6模块引入，如 let util = require('util_alg');;
-
-    直接调用ETool对象中得方法，如console.log(Util.isObject({});
-
-    Git 仓库地址 https://github.com/YangWenLong123/util.git
+### npm:
+``` bash
+$ npm install --save-dev util_alg
 ```
+
+webpack、RequireJS、SeaJS等
+
+``` javascript
+const Util = require('util_alg')
+const flag = Util.isObject({})
+```
+
+**推荐使用方法**
+
+``` javascript
+import Util from 'util_alg';
+const flag = Util.isObject({})
+```
+
+## API文档
+
+|    method                     |   Description       |
+|----------------|-----------------------------------------|--------------|-----------|
+|
+| Util.isObject()|  [是否为对象类型数据]
+| Util.isArray()|  [是否为数组类型数据]
+| Util.isBoolean()|  [是否为布尔类型数据]
+| Util.isString()|  [是否为字符类型数据]
+| Util.isNumber()|  [是否为数字类型数据]
+| Util.isUndefined()|  [是否为Undefined类型数据]
+| Util.isNull()|  [是否为Null类型数据]
+| Util.isFunction()|  [是否为函数类型数据]
+| Util.isSymboll()|  [是否为Symboll类型数据]
+| Util.isRegExp()|  [是否为正则类型数据]
+| Util.isError()|  [是否为Error类型数据]
+| Util.isDate()|  [是否为日期类型数据]
+| Util.isMath()|  [是否为Math类型数据]
+| Util.isGlobal()|  [是否为Global类型数据]
+| Util.isArrayBuffer()|  [是否为buffer类型数据]
+| Util.isBlob()|  [是否为Blob类型数据]
+| Util.isFinite()|  [是否为数字]
--- index.js
+++ index.js
@@ -15,11 +15,111 @@
 
     /**
      * @description 数据类型检测
+     * @return  {Boolean}
      */
     Util.isObject = function (a) {
         let type = Object.prototype.toString.call(a);
-        return type == '[object Object]'
+        return type == '[object Object]';
     }
+
+    Util.isArray = function (a) {
+        let type = Object.prototype.toString.call(a);
+        return type == '[object Array]';
+    }
+
+    Util.isBoolean = function (a) {
+        let type = Object.prototype.toString.call(a);
+        return type == '[object Boolean]';
+    }
+
+    Util.isString = function (a) {
+        let type = Object.prototype.toString.call(a);
+        return type == '[object String]';
+    }
+
+    Util.isNumber = function (a) {
+        let type = Object.prototype.toString.call(a);
+        return type == '[object Number]';
+    }
+
+    Util.isUndefined = function (a) {
+        let type = Object.prototype.toString.call(a);
+        return type == '[object Undefined]';
+    }
+
+    Util.isNull = function (a) {
+        let type = Object.prototype.toString.call(a);
+        return type == '[object Null]';
+    }
+
+    Util.isFunction = function (a) {
+        let type = Object.prototype.toString.call(a);
+        return type == '[object Function]';
+    }
+
+    Util.isSymboll = function (a) {
+        let type = Object.prototype.toString.call(a);
+        return type == '[object Symbol]';
+    }
+
+    Util.isRegExp = function (a) {
+        let type = Object.prototype.toString.call(a);
+        return type == '[object RegExp]';
+    }
+
+    Util.isError = function (a) {
+        let type = Object.prototype.toString.call(a);
+        return type == '[object Error]';
+    }
+
+    Util.isDate = function (a) {
+        let type = Object.prototype.toString.call(a);
+        return type == '[object Date]';
+    }
+
+    Util.isMath = function (a) {
+        let type = Object.prototype.toString.call(a);
+        return type == '[object Math]';
+    }
+
+    Util.isGlobal = function (a) {
+        let type = Object.prototype.toString.call(a);
+        return type == '[object global]';
+    }
+
+    Util.isArrayBuffer = function (a) {
+        return a instanceof ArrayBuffer;
+    }
+
+    Util.isBlob = function (a) {
+        return a instanceof Blob;
+    }
+
+    Util.isFinite = function (a) {
+        return Number.isFinite(a);
+    }
+
+    /**
+     * @description 时间函数
+     */
+
+    /**
+     * @description 节流
+     */
+
+    /**
+     * @description 防抖
+     */
+
+    /**
+     * @description 浏览器环境判断
+     */
+
+    /**
+     * @description 复制粘贴
+     */
+
+
 
     return Util;
 })));
